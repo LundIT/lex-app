b4520f29df7c902ecb763af06b6e2ecadbc0d388
hazempgm <30691209+hazempgm@users.noreply.github.com>
2024-09-12 14:32:20 +0200

    Reusing code and making the logs view more readable

--- lex/lex_app/rest_api/views/calculations/InitCalculationLogs.py
+++ lex/lex_app/rest_api/views/calculations/InitCalculationLogs.py
@@ -9,25 +9,17 @@ from django.http import JsonResponse
 
 from lex.lex_app.logging.CalculationLog import CalculationLog
 from lex.lex_app.logging.UserChangeLog import UserChangeLog
+from lex_app.rest_api.signals import get_model_data
 
 
 class InitCalculationLogs(APIView):
     http_method_names = ['get']
     permission_classes = [HasAPIKey | IsAuthenticated]
+
     def get(self, request, *args, **kwargs):
         calculation_record = request.query_params['calculation_record']
         calculation_id = request.query_params['calculation_id']
 
-        messages = []
+        logs = get_model_data(calculation_record, calculation_id)
 
-        # Fetch messages from UserChangeLog
-        queryset_ucl = UserChangeLog.objects.filter(calculation_record=calculation_record,
-                                                    calculationId=calculation_id).only('timestamp', 'message')
-        messages.extend(f"{message.timestamp} {message.message}" for message in queryset_ucl)
-
-        # Fetch messages from CalculationLog
-        queryset_calc = CalculationLog.objects.filter(calculation_record=calculation_record,
-                                                      calculationId=calculation_id).only('timestamp', 'message')
-        messages.extend(f"{message.timestamp} {message.message}" for message in queryset_calc)
-
-        return JsonResponse({"logs": "\n".join(messages)})
+        return JsonResponse({"logs": logs})
